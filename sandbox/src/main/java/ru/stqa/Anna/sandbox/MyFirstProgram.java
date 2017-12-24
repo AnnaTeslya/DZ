fc9f8bfc9664e42dfd6f7bd26407a4e4ad98d824
AnnaTeslya <teslyaanna1995@gmail.com>
2017-12-24 23:29:10 +0300

    Добавлено несколькой функций

--- sandbox/src/main/java/ru/stqa/Anna/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/Anna/sandbox/MyFirstProgram.java
@@ -2,8 +2,25 @@ package ru.stqa.Anna.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] agrs){
+	public static void main(String[] agrs) {
+		hello("user");
+		hello("Anna");
+		double l = 8.0;
+		double a=4;
+		double b=7;
+		System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
 
-		System.out.println( " Hello, world!");
 	}
+
+	public static void hello(String somebody) {
+		System.out.println(" Hello," + somebody +"!");
+	}
+	public static double area(double len) {
+		return len * len;
+	}
+	public static double area(double a, double b){
+		return a*b;
+	}
+
 }
