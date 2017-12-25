2e303de17d88e7f76e3d5f380998a4a364351516
AnnaTeslya <teslyaanna1995@gmail.com>
2017-12-25 04:37:42 +0300

    Вычисляли площадь квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/Anna/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/Anna/sandbox/MyFirstProgram.java
@@ -5,22 +5,18 @@ public class MyFirstProgram {
 	public static void main(String[] agrs) {
 		hello("user");
 		hello("Anna");
-		double l = 8.0;
-		double a=4;
-		double b=7;
-		System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
+
+		Square s = new Square(6);
+
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
 
 	}
-
 	public static void hello(String somebody) {
 		System.out.println(" Hello," + somebody +"!");
 	}
-	public static double area(double len) {
-		return len * len;
-	}
-	public static double area(double a, double b){
-		return a*b;
-	}
+}
+
 
 }
