9a1b9e6cc6f1b94b149bf6f4b9e67a909f76b0b2
AnnaTeslya <teslyaanna1995@gmail.com>
2018-01-02 02:18:08 +0300

    Лекция 2.3 Фикстуры: Перенесли часть когда из @Test в @BeforeMethod

--- addressbook-web-test/src/test/java/ru/stqa/Anna/addressbook/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/Anna/addressbook/GroupCreationTests.java
@@ -22,32 +22,30 @@ public class GroupCreationTests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void testGroupCreation() {
-        wd.get("http://localhost/addressbook/group.php");
-        wd.findElement(By.id("content")).click();
+        wd.get("http://localhost/addressbook/index.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testGroupCreation() {
+
+        wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("test2");
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).sendKeys("test");
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("test3");
+        wd.findElement(By.name("group_header")).sendKeys("testik");
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("test4");
+        wd.findElement(By.name("group_footer")).sendKeys("test5");
         wd.findElement(By.name("submit")).click();
         wd.findElement(By.linkText("group page")).click();
     }
