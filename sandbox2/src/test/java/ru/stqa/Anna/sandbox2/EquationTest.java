efebcdfd2b2520c76540a7d88a2054246a11f483
AnnaTeslya <teslyaanna1995@gmail.com>
2018-01-08 01:01:07 +0300

    Лекция 3.2

--- sandbox2/src/test/java/ru/stqa/Anna/sandbox2/EquationTest.java
+++ sandbox2/src/test/java/ru/stqa/Anna/sandbox2/EquationTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class EquationTest {
-@Test
+  @Test
   public void test0() {
     Equation e = new Equation(1, 1, 1);
     Assert.assertEquals(e.rootNumber(), 0);
@@ -18,5 +18,20 @@ public class EquationTest {
   public void test2() {
     Equation e = new Equation(1, 5, 6);
     Assert.assertEquals(e.rootNumber(), 2);
+  }
+
+  public void testLinear() {
+    Equation e = new Equation(0, 5, 6);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+
+  public void testConstant() {
+    Equation e = new Equation(0, 0, 6);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+
+  public void testZero() {
+    Equation e = new Equation(0, 0, 0);
+    Assert.assertEquals(e.rootNumber(), -1);
   }
 }
