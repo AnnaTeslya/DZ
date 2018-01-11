f2b17cd1f0b83747d93b126cf8d0ff1cdff54413
AnnaTeslya <teslyaanna1995@gmail.com>
2018-01-12 02:48:41 +0300

    Лекция 3.10

--- addressbook/src/test/java/ru/stqa/Anna/addressbook/tests/GroupDelationTest.java
+++ addressbook/src/test/java/ru/stqa/Anna/addressbook/tests/GroupDelationTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.Anna.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.Anna.addressbook.model.GroupDate;
 
 public class GroupDelationTest extends TestBase {
 
@@ -8,6 +9,9 @@ public class GroupDelationTest extends TestBase {
     public void testGroupDelation() {
 
         app.getNavigationHelper().gotoGroupPage();
+        if(!app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupDate("test101", null,null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnGroupPage();
