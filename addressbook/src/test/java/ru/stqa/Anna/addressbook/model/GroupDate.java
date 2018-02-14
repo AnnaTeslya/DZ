f2ce380381a2db31986c59d696dc797fdd71068c
AnnaTeslya <teslyaanna1995@gmail.com>
2018-02-15 01:48:53 +0300

    Лекция 4.8. Поиск максимального элемента в коллекции
    Улучшили тест создания группы

--- addressbook/src/test/java/ru/stqa/Anna/addressbook/model/GroupDate.java
+++ addressbook/src/test/java/ru/stqa/Anna/addressbook/model/GroupDate.java
@@ -1,27 +1,32 @@
 package ru.stqa.Anna.addressbook.model;
 
 public class GroupDate {
-  private final String id;
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  private  int id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupDate( String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
 
-  public GroupDate(String id, String name, String header, String footer) {
-    this.id = name;
+  public GroupDate(int id, String name, String header, String footer) {
+    this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getId() { return id;  }
+
+  public int getId() { return id;  }
 
   public String getName() {
     return name;
@@ -36,29 +41,28 @@ public class GroupDate {
   }
 
   @Override
+  public String toString() {
+    return "GroupDate{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
+  }
+  @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
 
     GroupDate groupDate = (GroupDate) o;
 
-    if (id != null ? !id.equals(groupDate.id) : groupDate.id != null) return false;
+    if (id != groupDate.id) return false;
     return name != null ? name.equals(groupDate.name) : groupDate.name == null;
   }
 
   @Override
   public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
+    int result = id;
     result = 31 * result + (name != null ? name.hashCode() : 0);
     return result;
-  }
-
-  @Override
-  public String toString() {
-    return "GroupDate{" +
-            "id='" + id + '\'' +
-            ", name='" + name + '\'' +
-            '}';
   }
 
 }
--- addressbook/src/test/java/ru/stqa/Anna/addressbook/tests/CroupCreationTest.java
+++ addressbook/src/test/java/ru/stqa/Anna/addressbook/tests/CroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.Anna.addressbook.model.GroupDate;
 
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -16,11 +17,22 @@ public class CroupCreationTest extends TestBase {
         List<GroupDate> before = app.getGroupHelper().getGroupList();
 
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupDate("test101", null,null));
+        GroupDate group = new GroupDate("test101", null,null);
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnGroupPage();
         List<GroupDate> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size() + 1, after.size());
+
+        int max=0;
+        for (GroupDate g: after){
+            if (g.getId()>max){
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     }
 
 }
