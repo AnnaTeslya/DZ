ea0bda589cdb247dafb5ba1b6ad21ae8f9734dce
AnnaTeslya <teslyaanna1995@gmail.com>
2018-02-15 00:55:18 +0300

    Лекция 4.7. Множества: неупорядоченные коллекции
    Дополнили тест для модификации групп

--- addressbook/src/test/java/ru/stqa/Anna/addressbook/tests/GroupModificationTest.java
+++ addressbook/src/test/java/ru/stqa/Anna/addressbook/tests/GroupModificationTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.Anna.addressbook.model.GroupDate;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -17,11 +18,16 @@ public class GroupModificationTest extends TestBase {
   List<GroupDate> before = app.getGroupHelper().getGroupList();
    app.getGroupHelper().selectGroup(before.size()-1);
    app.getGroupHelper().initGroupModification();
-   app.getGroupHelper().fillGroupForm(new GroupDate("Grouppaaa", null, "mOBILE"));
+   GroupDate group = new GroupDate(before.get(before.size()-1).getId(),"Grouppaaa", null, "mOBILE");
+   app.getGroupHelper().fillGroupForm(group);
    app.getGroupHelper().submitGroupModification();
    app.getGroupHelper().returnGroupPage();
   List<GroupDate> after = app.getGroupHelper().getGroupList();
   Assert.assertEquals(before.size(),after.size());
+
+  before.remove(before.size() -1);
+  before.add(group);
+  Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
  }
 
 
