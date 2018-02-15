205190e93556496fcfdca8ba0963e505b60ea21f
AnnaTeslya <teslyaanna1995@gmail.com>
2018-02-15 20:14:35 +0300

    Лекция 4.9. Лямбда-выражения (анонимные функции) и элементы функционального программирования

--- addressbook/src/test/java/ru/stqa/Anna/addressbook/tests/CroupCreationTest.java
+++ addressbook/src/test/java/ru/stqa/Anna/addressbook/tests/CroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.Anna.addressbook.model.GroupDate;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -17,20 +18,14 @@ public class CroupCreationTest extends TestBase {
         List<GroupDate> before = app.getGroupHelper().getGroupList();
 
         app.getGroupHelper().initGroupCreation();
-        GroupDate group = new GroupDate("test101", null,null);
+        GroupDate group = new GroupDate("test101", null, null);
         app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnGroupPage();
         List<GroupDate> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size() + 1, after.size());
 
-        int max=0;
-        for (GroupDate g: after){
-            if (g.getId()>max){
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     }
---
+++ sandbox2/src/main/java/ru/stqa/Anna/sandbox2/dz.java
@@ -0,0 +1,7 @@
+package ru.stqa.Anna.sandbox2;
+
+public class dz {
+  int massiv[] = new int[10];
+
+
+}
